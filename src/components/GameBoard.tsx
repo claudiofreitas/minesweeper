8ba99d610f8aefa5f1ab6ad3af048dfebcc46af3
Claudio Freitas <claudiofreitas3@gmail.com>
2022-06-27 02:46:50 +0900

    Center tile content

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 
 export const GameBoard: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="grid grid-cols-10 mx-auto my-0 bg-blue-300 w-80">
+    <div className="grid grid-cols-10 mx-auto my-0 bg-blue-300 w-max">
       {children}
     </div>
   );
--- src/components/GameTile.tsx
+++ src/components/GameTile.tsx
@@ -3,7 +3,7 @@ import { FC, PropsWithChildren } from 'react';
 export const GameTile: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div
-      className="m-auto my-auto mx-auto text-center outline-dashed outline-1 outline-amber-500 bg-red-400"
+      className="flex items-center justify-center m-auto my-auto mx-auto outline-dashed outline-1 outline-amber-500 bg-red-400"
       style={{
         width: 44,
         height: 44,
