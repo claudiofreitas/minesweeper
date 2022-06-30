58d9144298e2ca6cf8cdda8a088c8eebf59738c2
Claudio Freitas <claudiofreitas3@gmail.com>
2022-07-01 00:52:16 +0900

    Define TileData type

--- src/domain/TileData.ts
+++ src/domain/TileData.ts
@@ -1,8 +1,39 @@
-export interface TileData {
-  hasBomb: boolean;
-  isFlagged: boolean;
-  bombsAround: number;
+type TileType = 'bomb' | 'land';
+type LandTileState = 'covered' | 'flagged' | 'questioned' | 'discovered';
+type BombTileState =
+  | 'covered'
+  | 'flagged'
+  | 'questioned'
+  | 'discovered'
+  | 'exploded';
+
+// type TileState<T extends TileType> = T extends 'bomb'
+//   ? BombTileState
+//   : T extends 'land'
+//   ? LandTileState
+//   : never;
+
+type TileDataCommon = {
   x: number;
   y: number;
-  id: string;
-}
+  bombsAround: number;
+};
+type TileDataBomb = {
+  type: 'bomb';
+  state: BombTileState;
+};
+type TileDataLand = {
+  type: 'land';
+  state: LandTileState;
+};
+export type TileData = TileDataCommon & (TileDataBomb | TileDataLand);
+
+// TODO: improve type above
+// Would be best if the code below didn't offer the option 'exploded' for type 'land'
+// const tile: TileData = {
+//   x: 0,
+//   y: 0,
+//   bombsAround: 0,
+//   type: 'land',
+//   state: 'exploded',
+// };
