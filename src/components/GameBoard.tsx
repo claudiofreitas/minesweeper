57fde7e2705cadf9fb9bc9cff15d50b019aad5dd
Claudio Freitas <claudiofreitas3@gmail.com>
2022-07-03 02:59:06 +0900

    Add max-width to board

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -4,7 +4,7 @@ export const GameBoard: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div
       className="flex flex-col mx-auto w-max p-3 gap-3"
-      style={{ backgroundColor: '#111213', width: '90%' }}
+      style={{ backgroundColor: '#111213', width: '90%', maxWidth: '400px' }}
     >
       {children}
     </div>
